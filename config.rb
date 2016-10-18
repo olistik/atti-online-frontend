35f11a18aa58c9cb2c268671886a5e99b6cd50ee
Maurizio De Magnis <root@olisti.co>
2016-10-18 11:14:10 +0200

    fixes page urls

--- config.rb
+++ config.rb
@@ -27,11 +27,11 @@ end
 
 data.database.each do |section, years|
   slug_section = ConfigHelpers.slugify(section)
-  proxy "#{slug_section}", "/templates/section.html", locals: {section: section, years: years}, layout: "layout"
+  proxy "#{slug_section}/index.html", "/templates/section.html", locals: {section: section, years: years}, layout: "layout"
   years.each do |year, records|
-    proxy "#{slug_section}/#{year}", "/templates/year.html", locals: {section: section, year: year, records: records}, layout: "layout"
+    proxy "#{slug_section}/#{year}/index.html", "/templates/year.html", locals: {section: section, year: year, records: records}, layout: "layout"
     records.each do |number, record|
-      proxy "#{slug_section}/#{year}/#{number}", "/templates/record.html", locals: {section: section, year: year, record: record}, layout: "layout"
+      proxy "#{slug_section}/#{year}/#{number}/index.html", "/templates/record.html", locals: {section: section, year: year, record: record}, layout: "layout"
     end
   end
 end
